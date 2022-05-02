7c337c17922cdb8dc21d9c1f3b81bfba7374d0d2
Pedro Lima <62616647+retxeeD@users.noreply.github.com>
2022-05-01 21:02:04 -0300

    first commit

---
+++ criefesta/src/main/java/com/br/criefesta/CriefestaApplication.java
@@ -0,0 +1,13 @@
+package com.br.criefesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriefestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriefestaApplication.class, args);
+	}
+
+}
